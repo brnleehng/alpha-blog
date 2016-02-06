80b4c8c167ce1a2526b98132b4d6eabbc64fc656
brnleehng <l.brian.hoang@gmail.com>
2016-02-06 19:26:31 +0000

    Finished backend, now starting UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
+  
   def new
     @article = Article.new
   end
@@ -14,12 +19,33 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "article was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
   
-  private 
-  def article_params
-    params.require(:article).permit(:title, :description)
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "article was successfully deleted"
+    redirect_to articles_path
   end
+  
+  private 
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
